ca5be6af9f3d6889970fc63edd56ffb02988e463
venomaze <venomaze@protonmail.com>
2020-08-02 11:41:39 -0700

    Update token sandbox

--- sandbox/token.js
+++ sandbox/token.js
@@ -1,23 +1,49 @@
-const tokenHelper = require('../src/lib/token');
+const { SynchronizerPattern, HMACBasedPattern } = require('../src/lib/token');
 
+/**
+ * Synchronizer Token Pattern: using generator method directly
+ */
 (async () => {
-  const tokenLength = 32;
   const secret = 'secret_key';
-  const token = await tokenHelper.generateToken(tokenLength);
-  const signedToken = tokenHelper.signToken(token, secret);
-  const isValid = tokenHelper.verifyToken(token, signedToken, secret);
+  const token = await SynchronizerPattern.generateToken();
+  const signedToken = SynchronizerPattern.signToken(token, secret);
+  const isValid = SynchronizerPattern.verifyToken(token, signedToken, secret);
 
-  console.log(token, signedToken, isValid);
+  console.log(
+    'Synchronizer Token Pattern (direct):',
+    token,
+    signedToken,
+    isValid
+  );
 })();
 
+/**
+ * Synchronizer Token Pattern: using getToken method
+ */
 (async () => {
-  const tokenLength = 32;
   const secret = 'secret_key';
-  const { token, signedToken } = await tokenHelper.getToken(
-    secret,
-    tokenLength
-  );
-  const isValid = tokenHelper.verifyToken(token, signedToken, secret);
+  const {
+    serverToken: token,
+    clientToken: signedToken,
+  } = await SynchronizerPattern.getToken(secret);
+  const isValid = SynchronizerPattern.verifyToken(token, signedToken, secret);
 
-  console.log(token, signedToken, isValid);
+  console.log(
+    'Synchronizer Token Pattern (get method):',
+    token,
+    signedToken,
+    isValid
+  );
+})();
+
+/**
+ * HMAC Based Token Pattern
+ */
+(() => {
+  const secret = 'secret_key';
+  const sid = 'random_session_id';
+  const token = HMACBasedPattern.generateToken(sid, secret);
+  const isValid = HMACBasedPattern.verifyToken(token, sid, secret);
+
+  console.log('HMAC Based Token Pattern:', token, isValid);
 })();
