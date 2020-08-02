d5b99749ce78175262ce50746b96c1d8b5fbb246
venomaze <venomaze@protonmail.com>
2020-08-02 01:03:47 -0700

    Add comments to token methods

--- src/lib/token.js
+++ src/lib/token.js
@@ -1,6 +1,12 @@
 const crypto = require('crypto');
 const util = require('util');
 
+/**
+ * Generate a random token with the given length
+ * @access private
+ * @param {Number} [length] - The length of the token bytes (optional)
+ * @returns {String} - The generated token
+ */
 const generateToken = async (length = 16) => {
   const randomBytes = util.promisify(crypto.randomBytes);
   const bytes = await randomBytes(length);
@@ -9,6 +15,13 @@ const generateToken = async (length = 16) => {
   return token;
 };
 
+/**
+ * Sign the token with the given secret key
+ * @access private
+ * @param {String} token - The token which is going to be signed
+ * @param {String} secret - The secret which is used for signing
+ * @returns {String} - The signed key
+ */
 const signToken = (token, secret) => {
   const hash = crypto.createHmac('sha1', secret).update(token);
   const signedToken = hash.digest('hex');
@@ -16,6 +29,13 @@ const signToken = (token, secret) => {
   return signedToken;
 };
 
+/**
+ * Verify if the token is valid or not
+ * @param {String} token - The token to be verified
+ * @param {String} signedToken - The signed token to compare with
+ * @param {String} secret - The secret which is used for signed token
+ * @returns {Boolean} - True if the given token is valid
+ */
 const verifyToken = (token, signedToken, secret) => {
   const expected = signToken(token, secret);
   const isValid = expected === signedToken;
@@ -23,6 +43,12 @@ const verifyToken = (token, signedToken, secret) => {
   return isValid;
 };
 
+/**
+ * Generate and sign a new token
+ * @param {String} secret - The secret key which is used for signing tokens
+ * @param {Number} [tokenLength] - The length of the token bytes (optional)
+ * @returns {Object} - An object containing both generated and signed tokens
+ */
 const getToken = async (secret, tokenLength = 16) => {
   const token = await generateToken(tokenLength);
   const signedToken = signToken(token, secret);
