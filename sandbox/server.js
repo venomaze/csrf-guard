742beb2cf39187f8b12e677b2e74491c104ac747
venomaze <venomaze@protonmail.com>
2020-08-04 10:08:40 -0700

    Create the middleware

--- sandbox/server.js
+++ sandbox/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const session = require('express-session');
 
+const CSRFGuard = require('../src');
+
 const app = express();
 
 app.use(
@@ -9,8 +11,33 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.use(
+  new CSRFGuard({
+    secret: 'secret_key',
+  })
+);
+
+app.get('/', async (req, res) => {
+  const token = await req.getToken();
+  const form = `
+    <form action="/test" method="POST">
+      <input type="hidden" name="csrf_token" value="${token}" />
+      <input type="text" name="username" />
+      <input type="submit" />
+    </form>
+  `;
+
+  res.send(form);
+});
+
+app.post('/test', (req, res) => {
+  const isTokenValid = req.isTokenValid();
+  const message = isTokenValid ? 'The token is valid.' : 'Token is NOT valid.';
+
+  res.send(message);
 });
 
 app.listen(3000, () => {
--- src/index.js
+++ src/index.js
@@ -6,12 +6,104 @@ class CSRFGuard {
     this.synchronizer = options.synchronizer || true;
     this.expiryTime = options.expiryTime || null;
 
-    if (!this.secret) throw new Error('Secret key must be included.');
+    if (!this.secret) {
+      throw new Error('Secret key must be included.');
+    }
 
     return this.middleware.bind(this);
   }
 
-  async middleware(req, res, next) {}
+  async middleware(req, res, next) {
+    const getToken = async (forced = false) => {
+      if (!req.session) {
+        throw new Error('Session object is not available.');
+      }
+
+      /**
+       * Using Synchronizer Token Pattern
+       */
+      if (this.synchronizer) {
+        if (!forced) {
+          const token = req.session.csrf_token;
+
+          if (token) {
+            const signedToken = SynchronizerPattern.signToken(
+              token,
+              this.secret
+            );
+
+            return signedToken;
+          }
+        }
+
+        const { serverToken, clientToken } = await SynchronizerPattern.getToken(
+          this.secret
+        );
+
+        req.session.csrf_token = serverToken;
+
+        return clientToken;
+      }
+
+      /**
+       * HMAC Based Token Pattern
+       */
+      const sid = req.session.id;
+      const token = HMACBasedPattern.generateToken(sid, this.secret);
+
+      return token;
+    };
+
+    const isTokenValid = () => {
+      if (!req.session) {
+        throw new Error('Session object is not available.');
+      }
+
+      const clientToken =
+        (req.body && req.body.csrf_token) ||
+        (req.query && req.query.csrf_token) ||
+        req.get('csrf-token') ||
+        req.get('xsrf-token') ||
+        req.get('x-csrf-token') ||
+        req.get('x-xsrf-token');
+
+      if (!clientToken) return false;
+
+      /**
+       * Synchronizer Token Pattern
+       */
+      if (this.synchronizer) {
+        if (!req.session.csrf_token) return false;
+
+        const serverToken = req.session.csrf_token;
+        const isValid = SynchronizerPattern.verifyToken(
+          serverToken,
+          clientToken,
+          this.secret
+        );
+
+        return isValid;
+      }
+
+      /**
+       * HMAC Based Token Pattern
+       */
+      const sid = req.session.id;
+      const isValid = HMACBasedPattern.verifyToken(
+        clientToken,
+        sid,
+        this.secret,
+        this.expiryTime
+      );
+
+      return isValid;
+    };
+
+    req.getToken = getToken;
+    req.isTokenValid = isTokenValid;
+
+    return next();
+  }
 }
 
 module.exports = CSRFGuard;
